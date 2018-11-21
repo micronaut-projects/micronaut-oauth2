ab102cdc87667dd087cc51b2a3c920bb212de8ab
sdelamo <sergio.delamo@softamo.com>
2018-11-21 09:48:49 +0100

    Add ersatz test

--- src/test/groovy/io/micronaut/security/oauth2/openid/configuration/OpenIdConfigurationClientSpec.groovy
+++ src/test/groovy/io/micronaut/security/oauth2/openid/configuration/OpenIdConfigurationClientSpec.groovy
@@ -1,5 +1,7 @@
 package io.micronaut.security.oauth2.openid.configuration
 
+import com.stehno.ersatz.ContentType
+import com.stehno.ersatz.ErsatzServer
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.env.Environment
 import io.micronaut.context.exceptions.NoSuchBeanException
@@ -27,10 +29,27 @@ class OpenIdConfigurationClientSpec extends Specification {
 
     void "OpenIdConfigurationClient bean is loaded if micronaut.security.oauth2.openid-configuration is set"() {
         given:
+        String openIdConfigurationJson = 'src/test/resources/auth0-openid-configuration.json'
+        File jsonFile = new File(openIdConfigurationJson)
+        assert jsonFile.exists()
+
+        and:
+        ErsatzServer ersatz = new ErsatzServer()
+        ersatz.expectations {
+            get('/.well-known/openid-configuration'){
+                called 1
+                responder {
+                    body(jsonFile.text, ContentType.APPLICATION_JSON)
+                }
+            }
+        }
+
+        and:
+        String openIdConfigurationEndpoint = "${ersatz.httpUrl}/.well-known/openid-configuration"
         ApplicationContext context = ApplicationContext.run([
                 (SPEC_NAME_PROPERTY)                            : getClass().simpleName,
-                'micronaut.security.enabled': true,
-                'micronaut.security.oauth2.openid-configuration': 'https://micronaut.io'
+                'micronaut.security.enabled'                    : true,
+                'micronaut.security.oauth2.openid-configuration': openIdConfigurationEndpoint
         ], Environment.TEST)
 
         when:
@@ -39,7 +58,26 @@ class OpenIdConfigurationClientSpec extends Specification {
         then:
         noExceptionThrown()
 
+        when:
+        context.getBean(OpenIdConfigurationFactory)
+
+        then:
+        noExceptionThrown()
+
+        // You need to attempt to retrieve a class bean created by the factory in order for factory method to kick off
+        when:
+        context.getBean(OpenIdConfiguration)
+
+        then:
+        noExceptionThrown()
+
+        and:
+        ersatz.verify()
+
         cleanup:
+        ersatz.stop()
+
+        and:
         context.close()
     }
 }
