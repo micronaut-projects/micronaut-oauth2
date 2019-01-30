cd5f4168d8c9c55ced4034169bb2de178279cea8
sdelamo <sergio.delamo@softamo.com>
2019-01-30 12:41:45 +0100

    Simplify travis-build

--- travis-build.sh
+++ travis-build.sh
@@ -23,12 +23,6 @@ if [ "${TRAVIS_JDK_VERSION}" == "openjdk11" ] ; then
     exit $EXIT_STATUS
 fi
 
-
-git config --global user.name "$GIT_NAME"
-git config --global user.email "$GIT_EMAIL"
-git config --global credential.helper "store --file=~/.git-credentials"
-echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
-
 if [[ $EXIT_STATUS -eq 0 ]]; then
     if [[ -n $TRAVIS_TAG ]]; then
         echo "Skipping Tests to Publish Release"
@@ -37,6 +31,10 @@ if [[ $EXIT_STATUS -eq 0 ]]; then
         ./gradlew --stop
         ./gradlew testClasses --no-daemon || EXIT_STATUS=$?
 
+        if [ $EXIT_STATUS -ne 0 ]; then
+            exit $EXIT_STATUS
+        fi
+
         ./gradlew --stop
         ./gradlew check --no-daemon || EXIT_STATUS=$?
     fi
@@ -44,32 +42,50 @@ fi
 
 if [[ $EXIT_STATUS -eq 0 ]]; then
     echo "Publishing archives for branch $TRAVIS_BRANCH"
+
     if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH =~ ^master$ && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+        echo "Publishing archives"
+        ./gradlew --stop
 
-      echo "Publishing archives"
-      ./gradlew --stop
-      if [[ -n $TRAVIS_TAG ]]; then
-          ./gradlew bintrayUpload --no-daemon --stacktrace || EXIT_STATUS=$?
-          if [[ $EXIT_STATUS -eq 0 ]]; then
-            ./gradlew synchronizeWithMavenCentral --no-daemon
-          fi
-      else
-          ./gradlew publish --no-daemon --stacktrace || EXIT_STATUS=$?
-      fi
+        if [[ -n $TRAVIS_TAG ]]; then
+            ./gradlew bintrayUpload --no-daemon --stacktrace || EXIT_STATUS=$?
+            if [ $EXIT_STATUS -ne 0 ]; then
+                exit $EXIT_STATUS
+            fi
 
-      if [[ $EXIT_STATUS -eq 0 ]]; then
-       ./gradlew --console=plain --no-daemon docs  || EXIT_STATUS=$?
+            ./gradlew synchronizeWithMavenCentral --no-daemon || EXIT_STATUS=$?
 
-        git clone https://${GH_TOKEN}@github.com/micronaut-projects/micronaut-grpc.git -b gh-pages gh-pages --single-branch > /dev/null
+            if [ $EXIT_STATUS -ne 0 ]; then
+                exit $EXIT_STATUS
+            fi
+        else
+            ./gradlew publish --no-daemon --stacktrace || EXIT_STATUS=$?
+
+            if [ $EXIT_STATUS -ne 0 ]; then
+                exit $EXIT_STATUS
+            fi
+        fi
+
+        git config --global user.name "$GIT_NAME"
+        git config --global user.email "$GIT_EMAIL"
+        git config --global credential.helper "store --file=~/.git-credentials"
+        echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+
+        ./gradlew --console=plain --no-daemon docs  || EXIT_STATUS=$?
+
+        if [ $EXIT_STATUS -ne 0 ]; then
+            exit $EXIT_STATUS
+        fi
+
+        git clone https://${GH_TOKEN}@github.com/micronaut-projects/micronaut-oauth2.git -b gh-pages gh-pages --single-branch > /dev/null
 
         cd gh-pages
 
         # If this is the master branch then update the snapshot
         if [[ $TRAVIS_BRANCH =~ ^master|[12]\..\.x$ ]]; then
-           mkdir -p snapshot
-           cp -r ../build/docs/. ./snapshot/
-
-           git add snapshot/*
+            mkdir -p snapshot
+            cp -r ../build/docs/. ./snapshot/
+            git add snapshot/*
         fi
 
         # If there is a tag present then this becomes the latest
@@ -99,9 +115,6 @@ if [[ $EXIT_STATUS -eq 0 ]]; then
         cd ..
 
         rm -rf gh-pages
-      fi
- 
-
     fi
 fi
 
